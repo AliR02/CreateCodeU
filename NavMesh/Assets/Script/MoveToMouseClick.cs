312752b0e17bb139e0be4717053045aea58e699a
AliR02 <alison.n.ramirez@lions.enc.edu>
2023-09-26 23:41:05 -0400

    navmesh

---
+++ NavMesh/Assets/Script/MoveToMouseClick.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToMouseClick : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
